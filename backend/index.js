fa7bd43b1ea7624137edae9c5f16c289184514a6
Irtaza <irtazanaqvi05@gmail.com>
2024-07-13 02:16:57 +0500

    Added middleware and fixed bugs

--- backend/index.js
+++ backend/index.js
@@ -14,6 +14,76 @@ const app = express();
 const jwtSecretKey = "jwt-secret-key";
 
 // Middleware for CORS and JSON parsing
+app.use(
+  cors({
+    origin: [
+      "https://deploy-portal-frontend.vercel.app",
+      "http://localhost:3006",
+      "https://pteu-data.mujtabamehdi.com",
+    ], // Allow all origins
+    methods: ["POST", "GET", "PUT", "DELETE", "OPTIONS"],
+    credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+app.use(express.json());
+app.use(cookieParser());
+
+// MongoDB connection
+const mongoURI =
+  "mongodb+srv://irtaza:N3VKunF1B32TLC2u@cluster0.clurvxu.mongodb.net/PakistanTechiesInEurope?retryWrites=true&w=majority&appName=Cluster0";
+mongoose
+  .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("MongoDB connected...");
+  })
+  .catch((err) => {
+    console.log("Error connecting to MongoDB:", err.message);
+  });
+
+// Middleware to verify JWT
+const verifyUser = (req, res, next) => {
+  const token = req.cookies.token;
+  if (!token) {
+    return res.json("You are not authenticated");
+  } else {
+    jwt.verify(token, jwtSecretKey, (err, decoded) => {
+      if (err) {
+        return res.json("You are not authenticated");
+      } else {
+        req.user = decoded;
+        next();
+      }
+    });
+  }
+};
+
+// User login
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+  UserModel.findOne({ email: email }).then((user) => {
+    if (user) {
+      bcrypt.compare(password, user.password, (err, response) => {
+        if (response) {
+          const token = jwt.sign({ email: user.email }, jwtSecretKey, {
+            expiresIn: "1d",
+          });
+          res.cookie("token", token, {
+            httpOnly: true,
+            secure: true, // Set secure to true if using HTTPS
+            sameSite: "None",
+          });
+          res.json("Successfully Logged In");
+        } else {
+          res.json("Invalid Password");
+        }
+      });
+    } else {
+      res.json("Not Registered");
+    }
+  });
+});
 
 // User registration
 app.post("/register", (req, res) => {
