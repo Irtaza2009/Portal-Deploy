3baed038392c5e6d1b669ffb290d8d1f345a5174
Irtaza <irtazanaqvi05@gmail.com>
2024-07-13 14:13:11 +0500

    Changed local to deployed

--- backend/index.js
+++ backend/index.js
@@ -71,7 +71,7 @@ app.post("/login", (req, res) => {
           });
           res.cookie("token", token, {
             httpOnly: true,
-            //secure: true, // Set secure to true if using HTTPS
+            secure: true, // Set secure to true if using HTTPS
             sameSite: "None",
           });
           res.json("Successfully Logged In");
